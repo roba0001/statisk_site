2f785d6661fd1ad06f0e06fd72f09ee0feb2988f
Ronja <roba0001@stud.kea.dk>
2024-02-05 16:49:13 +0100

    mangler bare pris med rabat

--- productlist.js
+++ productlist.js
@@ -8,31 +8,28 @@ function showProducts(products) {
 }
 
 function showProduct(product) {
-  //   console.log(product);
+  console.log("product.discount:", product.discount);
   //fang template
   const template = document.querySelector("#smallProductTemplate").content;
   // lav en kopi
   const copy = template.cloneNode(true);
   // ændre indhold
   copy.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/1000/${product.id}.webp`;
-
   copy.querySelector("h3").textContent = product.productdisplayname;
-  //   variabel for subtle text
-  let subtleText = product.articletype + " - " + product.brandname;
-  copy.querySelector("p.subtle").textContent = subtleText;
+
+  copy.querySelector("p.subtle").textContent = product.articletype + " - " + product.brandname;
   //   pris og discount
   copy.querySelector("p.price").textContent = product.price + ",- DKK";
-  copy.querySelector(".discounted").textContent = product.discount;
+  copy.querySelector("p.discountpr").textContent = product.discount + " %";
 
   if (product.soldout) {
     // produktet er udsolgt
     copy.querySelector("article").classList.add("soldOut");
   }
-  if (product.onSale) {
-    // produktet er on sale
-    copy.querySelector("p.price").textContent = product.price;
-
-    copy.querySelector("div.discounted").textContent = product.discount;
+  if (product.discount === null) {
+    // produktet er ikke on sale
+    console.log("her er der intet salg");
+    copy.querySelector(".discountpr").classList.add("hide");
   }
   // appende
   document.querySelector("main").appendChild(copy);
