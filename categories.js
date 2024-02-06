c7ea10b22fd43765797c556a719375c5653cf1ed
Ronja <roba0001@stud.kea.dk>
2024-02-06 19:28:16 +0100

    sletta categories js

--- categories.js
+++
@@ -1,8 +0,0 @@
-copy.querySelector(".brands").setAttribute("href", `kategori.html?id=${product.id}`);
-
-const urlParams = new URLSearchParams(window.location.search);
-const brands = urlParams.get("brandname");
-
-fetch("https://kea-alt-del.dk/t7/api/products/" + brands)
-  .then((response) => response.json())
-  .then((data) => showProduct(data));
