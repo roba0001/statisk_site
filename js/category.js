c09748b9b673728d23e092c45ee5d1fc33f782c5
Ronja <roba0001@stud.kea.dk>
2024-02-08 10:51:39 +0100

    kategori DONE

--- js/category.js
+++ js/category.js
@@ -1,4 +1,4 @@
-fetch("https://kea-alt-del.dk/t7/api/products/")
+fetch("https://kea-alt-del.dk/t7/api/categories")
   .then((response) => response.json())
   .then((data) => showCategories(data));
 
@@ -7,15 +7,15 @@ function showCategories(categories) {
   categories.forEach(showCategory);
 }
 
-function showCategory(product) {
-  console.log(product);
+function showCategory(cat) {
+  console.log(cat);
   // fang template
   const template = document.querySelector("#txt_kategori").content;
   // lav en kopi
   const copy = template.cloneNode(true);
   // ændre indhold
-  copy.querySelector("button").textContent = "denne kategori er " + product.category;
-  copy.querySelector("button").setAttribute("href", `produktliste.html?category=${product.category}`);
+  copy.querySelector("a").textContent = cat.category;
+  copy.querySelector("a").setAttribute("href", `produktliste.html?category=${cat.category}`);
   // vælg parent
   // append (tilføj kopi til html)
   document.querySelector("main").appendChild(copy);
